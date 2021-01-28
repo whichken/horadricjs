0add04abc5e68fbe53f130c11cf46aef20e46a82
whichken <23066703+whichken@users.noreply.github.com>
2021-01-28 16:16:25 -0500

    support directory paths in manual

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { join } from "path"
+import { lstatSync, existsSync, readdirSync } from "fs"
 import express, { json } from "express"
 
 import { logger } from "./logger"
-import { verifyConfigExists } from "./settings"
+import { getProfile, getSourcePath, verifyConfigExists } from "./settings"
 import { Queue } from "./queue"
 import { SonarrEvent } from "./schemas/sonarr"
 import { RadarrEvent } from "./schemas/radarr"
@@ -31,7 +32,21 @@ app.post("/manual(/:profile)?", (req, res) => {
 
   if (!event.path) return res.status(400).json({ error: "Must provide path" })
 
-  queue.process(event.path, req.params.profile)
+  // Allow directories instead of just a file
+  const profile = getProfile(req.params.profile)
+  let path = event.path
+  for (const mapping of profile.pathMappings || [])
+    if (path.startsWith(mapping.from)) path = path.replace(mapping.from, mapping.to)
+  path = getSourcePath(path)
+
+  if (!existsSync(path)) return res.status(400).json({ error: "Path doesn't exist" })
+
+  if (lstatSync(path).isDirectory()) {
+    const files = readdirSync(path)
+    files.forEach(file => queue.process(file, req.params.profile))
+  } else {
+    queue.process(event.path, req.params.profile)
+  }
 
   return res.status(204).send()
 })
